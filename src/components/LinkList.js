6e42fbc06a45d9e82839707093d3aafcf3b0a3c8
fv17 <forest.valley0417@gmail.com>
2019-08-10 16:11:59 +0900

    feat: Add feed query

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react'
+
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
 import Link from './Link'
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`
 
 class LinkList extends Component {
   render() {
-    const links = [
-      {
-        id: 1,
-        description: "description1",
-        url: "url1",
-      },
-      {
-        id: 2,
-        description: "description2",
-        url: "url2",
-      },
-    ]
-
     return(
-      <div>{links.map(link => <Link key={link.id} link={link} />)}</div>
+      <Query query={FEED_QUERY}>
+        {
+          ({ loading, error, data }) => {
+            if (loading) return <div>Fetching</div>
+            if (error) return <div>Error</div>
+
+            const links = data.feed.links
+
+            return(
+              <div>
+                {links.map(link => <Link key={link.id} link={link} />)}
+              </div>
+            )
+          }
+        }
+      </Query>
     )
   }
 }
